29c9972a2b081cab9ba0e4c360bf0e6ed9428d92
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-04-26 10:56:14 -0500

    alien 4

---
+++ src/Aliens/TestConstructores.java
@@ -0,0 +1,8 @@
+package Aliens;
+
+public class TestConstructores {
+    public static void main(String[] args) {
+        Alien a = new Alien(100, "Azul"); // Se ajustará a 30
+        a.imprimir();
+    }
+}
