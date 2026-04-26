2ffe949f7ee67e95d9271470d0c912b4228e0128
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-04-26 11:00:19 -0500

    brazos y piernas

--- src/Aliens/Alien.java
+++ src/Aliens/Alien.java
@@ -24,13 +24,32 @@ public class Alien {
         this.precioOjo = this.tamanio * 0.05;
         this.precioTotal = 0;
     }
+    
+    public boolean agregarBrazos(int cantidad) {
+        if ((this.numeroBrazos + this.numeroPies + cantidad) <= 10) {
+            this.numeroBrazos += cantidad;
+            calcularPrecioTotal();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean agregarPiernas(int cantidad) {
+        if ((this.numeroBrazos + this.numeroPies + cantidad) <= 10) {
+            this.numeroPies += cantidad;
+            calcularPrecioTotal();
+            return true;
+        }
+        return false;
+    }
+    
     public int getTamanio() { return tamanio; }
     public String getColor() { return color; }
     public int getNumeroOjos() { return numeroOjos; }
     public int getNumeroBrazos() { return numeroBrazos; }
     public int getNumeroPies() { return numeroPies; }
     public double getPrecioTotal() { return precioTotal; }
-
+    
     public void imprimir() {
         System.out.println("Alien: " + color + " | Tamaño: " + tamanio + "cm");
         System.out.println("Precio Total: $" + precioTotal);
