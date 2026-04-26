c181fd2ebdfed489d26608b94f0904b60cee5356
joshTer-c2007 <jhosep.07txx@gmail.com>
2026-04-26 11:03:15 -0500

    Taller Complet

--- src/Aliens/AlienTest.java
+++ src/Aliens/AlienTest.java
@@ -9,4 +9,12 @@ class AlienTest {
         Alien a = new Alien(2, "Verde");
         assertEquals(5, a.getTamanio());
     }
+    @Test
+    void testTodo() {
+        Alien a = new Alien(10, "Gris");
+        a.agregarBrazos(2);
+        a.agregarOjos(1);
+        // Precio: Cuerpo(2.0) + 2 Extremidades(2.0) + 1 Ojo(0.5) = 4.5
+        assertEquals(4.5, a.getPrecioTotal());
+    }
 }
